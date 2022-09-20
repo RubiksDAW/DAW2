3991a5f235c4b8650cc237d8d60e484d1d83cc12
Alejandro <aledelarosa2@gmail.com>
2022-09-20 11:10:15 +0200

    martes

---
+++ DWEC/PruebaPalindromo/main.js
@@ -0,0 +1,13 @@
+let numero = prompt("Dame un numero");
+let numeroReverso = numero.split('').reverse().join('');
+
+numero == numeroReverso? alert("Es palindromo") : alert("No es palindromo");
+
+
+
+
+
+
+
+
+
---
+++ DWEC/TareasJsBasicas/Actividades1-4.js
@@ -0,0 +1,23 @@
+// Tarea 1
+alert("Soy JavaScript")
+
+// Tarea 2
+let admin = ""
+let name = ""
+
+name = "John";
+admin = name;
+
+alert(admin);
+
+// Tarea 3
+
+const PLANETA = "Tierra"
+
+let usuarioActual = "Manuel"
+
+// Tarea 4
+
+/** Para birthday sería correcto el uso de mayúsculas, ya que es un valor que no se va a modificar. 
+ * Por el contrario age es una variable que será modificada y tiene sentido que sea en minúscula.
+ */
