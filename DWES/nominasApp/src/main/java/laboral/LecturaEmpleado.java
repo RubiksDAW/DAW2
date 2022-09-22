b164b519d84ac9f8778595d71d65bb7ea5c0e052
Alejandro <aledelarosa2@gmail.com>
2022-09-22 21:04:54 +0200

    Clase Lectura Empleado creada

--- DWES/nominasApp/src/main/java/laboral/LecturaEmpleado.java
+++ DWES/nominasApp/src/main/java/laboral/LecturaEmpleado.java
@@ -1,5 +1,9 @@
 package laboral;
 
+import java.io.File;
+
 public class LecturaEmpleado {
-    
+    public static void main(String[] args) {
+        File doc = new File("C:\\usuarios\\Alejandro\\Escritorio\\empleados.txt");
+    }
 }
