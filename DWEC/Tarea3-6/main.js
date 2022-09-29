5674aae5accfd7cece2de49db5cf164f0a68adc9
Alejandro <aledelarosa2@gmail.com>
2022-09-29 13:03:23 +0200

    ejercicios 3-6

---
+++ DWEC/Tarea3-6/main.js
@@ -0,0 +1,103 @@
+// Ejercicio 3
+
+function cadenaAlterna(cadena) {
+    let nuevaCadena = "";
+    let longitud = cadena.length;
+    console.log(longitud);
+    for (let i = 0; i < longitud; i++) {
+
+        if(i % 2 == 0){
+            nuevaCadena += cadena[i].toUpperCase();
+        }else{
+            nuevaCadena += cadena[i].toLowerCase();
+        }
+
+    }
+    return nuevaCadena;
+
+}
+
+console.log(cadenaAlterna("hola"));
+
+// Ejercicio 4 AÑADIR MAS PALABRAS
+
+function comprobarPalabra(cadena, palabra) {
+
+    if (cadena.indexOf(palabra,0) >= 0) {
+        alert("si contiene la palabra")
+    } else {
+        alert("no contine la palabra")
+    }
+
+}
+
+console.log(comprobarPalabra("El zorro morro", "morro"));
+
+// Ejercicio 4 version larga
+
+function comprobarPalabras(cadena, ...palabras) {
+
+    for (let  palabra of palabras) {
+
+        if (cadena.indexOf(palabra,0) >= 0) {
+            alert("si contiene la palabra")
+        } else {
+            alert("no contine la palabra")
+        }
+
+    }
+
+}
+
+console.log(comprobarPalabras("El zorro morro", "morro","zorro","El"));
+
+// Ejercicio 5 
+
+function truncar(texto, longitud) {
+
+    let palabras = texto.split(' ');
+    let nuevoTexto = [];
+    console.log(palabras);
+
+    for (let i = 0; i < palabras.length; i++) {
+
+        if (palabras[i].length >= longitud) {
+
+            nuevoTexto.push(palabras[i].substring(0, longitud - 1) + "...");
+            console.log(nuevoTexto);
+
+        } else {
+
+            nuevoTexto.push(palabras[i]);
+        }
+
+    }
+
+    return nuevoTexto.join(' ');
+
+
+}
+
+console.log(truncar("mamarracho el que lo lea", 3));
+
+// Ejercicio 6
+
+function extraerValor(str) {
+    let nuevaStr = "";
+    for (let i = 0; i < str.length; i++) {
+        
+        if(str[i] == '€' || str[i] == '.'){
+            nuevaStr += ''
+        }else{
+            nuevaStr += str[i]
+        }
+        
+    }
+    // SOLUCION MÁS OPTIMA
+    // return str.replace(/[€.]/g, '')
+    return nuevaStr;
+    
+}
+
+console.log(extraerValor("€120."));
+
