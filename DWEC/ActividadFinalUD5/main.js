6abcc24376268c0fe898c283ef6ec3a7af6f8ef5
Alejandro <aledelarosa2@gmail.com>
2022-10-10 18:30:16 +0200

    añadido actividad5 final

---
+++ DWEC/ActividadFinalUD5/main.js
@@ -0,0 +1,14 @@
+let valor ;
+
+let ventana = () => {
+    window.open("nueva.html","nuevaVentana","height=300, width=600")
+    
+}
+
+
+
+function moverIzquierda() {
+    
+    valor = document.getElementById('size').value;
+    
+}
