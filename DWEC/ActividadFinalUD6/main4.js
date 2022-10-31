7bb065f75be3e6d1473674161538bf585167ed39
Alejandro <aledelarosa2@gmail.com>
2022-10-31 08:29:30 +0100

    cambios menores

--- DWEC/ActividadFinalUD6/main4.js
+++ DWEC/ActividadFinalUD6/main4.js
@@ -1,7 +1,16 @@
 // Ejercicio 4
 
-function Factura(cliente = {
-    
-},{},{}) {
-    
+function Cliente(nombre,telefono,factura){
+    this.nombre = nombre
+    this.telefono = telefono
+    this.factura = factura
 }
+
+function factura(nombre,telefono,factura){
+    this.cliente =  new Cliente(nombre,telefono,factura)
+
+}
+
+let prueba = new factura("Manuel",12345,"Pruebafactura")
+
+console.log(prueba);
