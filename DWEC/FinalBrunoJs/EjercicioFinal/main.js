fb18d3144d39d0571859d4cf09c048f8adbf1ed9
Alejandro <aledelarosa2@gmail.com>
2022-11-13 21:07:08 +0100

    Creado json

---
+++ DWEC/FinalBrunoJs/EjercicioFinal/main.js
@@ -0,0 +1,24 @@
+
+const table = document.getElementsByClassName("crud-table")
+const form = document.getElementsByClassName("crud-form")
+const title = document.getElementsByClassName("crud-title")
+const template = document.getElementById("crud-template")
+const fragment = document.createDocumentFragment();
+const getAll = async () => {
+
+    try {
+
+        let res = await fetch('https://reqres.in/api/users/'),
+        json = await res.json();
+
+        if(!res.ok)throw {status:res.status, statusText: res.statusText}
+        console.log(json);
+
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded",getAll)
+
