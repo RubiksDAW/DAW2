a0df2bbc2026623e3d208c4dfd92a0702a63558f
Alejandro <aledelarosa2@gmail.com>
2022-11-13 21:11:25 +0100

    .

--- DWEC/FinalBrunoJs/EjercicioFinal/main.js
+++ DWEC/FinalBrunoJs/EjercicioFinal/main.js
@@ -4,16 +4,17 @@ const form = document.getElementsByClassName("crud-form")
 const title = document.getElementsByClassName("crud-title")
 const template = document.getElementById("crud-template")
 const fragment = document.createDocumentFragment();
+
+
 const getAll = async () => {
 
     try {
 
-        let res = await fetch('https://reqres.in/api/users/'),
-        json = await res.json();
+        let res = await fetch('https://reqres.in/api/users/')
+        let json = await res.json();
 
         if(!res.ok)throw {status:res.status, statusText: res.statusText}
         console.log(json);
-
     
     } catch (error) {
         console.log(error);
