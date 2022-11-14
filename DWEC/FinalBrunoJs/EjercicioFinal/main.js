f79ea8c97988eda212bbba3b6f7e89b220a123d0
Alejandro <aledelarosa2@gmail.com>
2022-11-14 11:53:49 +0100

    get completo

--- DWEC/FinalBrunoJs/EjercicioFinal/main.js
+++ DWEC/FinalBrunoJs/EjercicioFinal/main.js
@@ -4,6 +4,10 @@ const title = document.getElementById("crud-title")
 const template = document.getElementById("crud-template")
 const fragment = document.createDocumentFragment();
 
+function prueba() {
+    event.preventDefault()
+    document.body.innerHTML = ""
+}
 
 const getAll = async () => {
 
@@ -11,7 +15,7 @@ const getAll = async () => {
 
         let res = await fetch('https://reqres.in/api/users/')
         let json = await res.json();
-
+        
         if (!res.ok) throw { status: res.status, statusText: res.statusText }
 
         console.log(json.data);
@@ -27,12 +31,14 @@ const getAll = async () => {
             let img = document.createElement("img")
             img.setAttribute("src",elem.avatar)
 
+            let id = elem.id
+            
             let eliminar = document.createElement("button")
             let editar = document.createElement("button")
 
             eliminar.textContent = "Eliminar"
             editar.textContent = "Editar"
-
+            
             let marco = document.createElement("div")
             marco.setAttribute("class", "marco")
             marco.append(nombre)
@@ -40,7 +46,7 @@ const getAll = async () => {
             marco.append(img)
             marco.append(eliminar)
             marco.append(editar)
-            
+
             panelUsuarios.append(marco)
             console.log(nombre);
             console.log(mail);
@@ -50,7 +56,10 @@ const getAll = async () => {
             //   panelUsuarios.append(mail)
             //   panelUsuarios.append(img)
 
-        })
+        }
+
+        
+        )
 
     } catch (error) {
         console.log(error);
