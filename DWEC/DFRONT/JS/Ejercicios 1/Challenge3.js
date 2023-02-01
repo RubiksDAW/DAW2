43d2b7e8c76c706b0e8114c5a74a6c9b0f5bd6af
Alejandro De La Rosa Cosano <adelarco@emeal.nttdata.com>
2023-02-01 14:00:41 +0100

    nivel

---
+++ DWEC/DFRONT/JS/Ejercicios 1/Challenge3.js
@@ -0,0 +1,12 @@
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+  };
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his bestfriend is ${jonas.friends[0]}`)
