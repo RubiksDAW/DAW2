f45d1635a2f40d28a383e6b8a1b98d79669a92f6
Alejandro De La Rosa Cosano <adelarco@emeal.nttdata.com>
2023-02-17 19:41:54 +0100

    nivel

---
+++ DWEC/DFRONT/Firebase/PrimeraAppFirebase/initialize.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+
+      var firebaseConfig = {
+        apiKey: "AIzaSyCqyArgj7KdvwpMeLS5_gfMjOxMOt2ndYU",
+
+        authDomain: "todo-list-6ebfe.firebaseapp.com",
+
+        projectId: "todo-list-6ebfe",
+
+        storageBucket: "todo-list-6ebfe.appspot.com",
+
+        messagingSenderId: "816229595748",
+
+        appId: "1:816229595748:web:e41dfa325f5b8dfeb1bd71",
+      };
+      
+const app = initializeApp(firebaseConfig);
