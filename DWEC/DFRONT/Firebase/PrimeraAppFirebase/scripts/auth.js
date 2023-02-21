e2f9f500396e84ee5c7e36b5b5bc07dcd917c0ed
Alejandro De La Rosa Cosano <adelarco@emeal.nttdata.com>
2023-02-21 14:08:09 +0100

    tarea clon

---
+++ DWEC/DFRONT/Firebase/PrimeraAppFirebase/scripts/auth.js
@@ -0,0 +1,22 @@
+import { resetUI } from "./app.js";
+import { loadTareas } from "./app.js";
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider();
+
+auth.lenguageCode = "es";
+
+export async function login(){
+  try {
+      const response = await auth.signInWithPopup(provider);
+      console.log(response);
+      loadTareas()
+      return response.user;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export function logout(){
+  auth.signOut();
+  resetUI()
+}
